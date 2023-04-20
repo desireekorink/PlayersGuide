e7e91eb71c370b1e7015cdf1ca2b305c8e4bc28a
Desiree <desireekorink@hotmail.com>
2023-04-20 12:10:14 +0200

    Reformatted Countdown to make it readable 2023-04-20

--- src/PlayersGuidePackage/Files/Countdown.java
+++ src/PlayersGuidePackage/Files/Countdown.java
@@ -5,14 +5,14 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Countdown {
-        public static void main(String[] args) {
-            List<Integer> numbersList = Arrays.asList(11,10,9,8,7,6,5,4,3,2,1);
-            for (int number : numbersList) {
-                number--;
-                System.out.println(number);
-            }
-
-        }}
+    public static void main(String[] args) {
+        List<Integer> numbersList = Arrays.asList(11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1);
+        for (int number : numbersList) {
+            number--;
+            System.out.println(number);
+        }
+    }
+}
 
 
 /*
