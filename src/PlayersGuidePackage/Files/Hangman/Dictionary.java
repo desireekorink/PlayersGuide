a02ecb5ff003fa78c48999de40bf047d08f1e46d
Desiree <desireekorink@hotmail.com>
2023-07-27 14:29:10 +0200

    Edited dictonary.txt + woordenboek.txt for HangmanGame.java

---
+++ src/PlayersGuidePackage/Files/Hangman/Dictionary.java
@@ -0,0 +1,7 @@
+package PlayersGuidePackage.Files.Hangman;
+
+public class Dictionary {
+    //imports file / enum with words, (later on) translation and meaning.
+    //can select random from File
+
+}
