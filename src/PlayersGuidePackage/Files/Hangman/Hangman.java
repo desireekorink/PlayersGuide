50a6f5cd5d20aea0e306b4d1d0b0f1c7140184df
Desiree <desireekorink@hotmail.com>
2023-08-17 14:07:06 +0200

    Created a do-while loop in main, Added Hangman.getChoiceFromPlayer, Renamed Player.chooseLanguage to Player.getChoice, Renamed kiesTaal to languageChoice,

--- src/PlayersGuidePackage/Files/Hangman/Hangman.java
+++ src/PlayersGuidePackage/Files/Hangman/Hangman.java
@@ -8,22 +8,27 @@ public class Hangman {
 
     //Start Game
     public static void main(String[] args) throws FileNotFoundException {
+
         Hangman hangman = new Hangman();
         Player player = new Player();
-        Game game = new Game();
-        Language language = hangman.getLanguageFromPlayer(player);
+        //Keeps track of Game
+        do {
+            Game game = new Game();
+            Language language = hangman.getLanguageFromPlayer(player);
 
-        hangman.setLanguageToPlayerChoice(hangman, game, language);
+            hangman.setLanguageToPlayerChoice(hangman, game, language);
 
-        String word = hangman.dictionary.getWord();
-        game.setWord(word);
+            String word = hangman.dictionary.getWord();
+            game.setWord(word);
 
-        while (game.isActive()) {
-            System.out.print(hangman.report.showReport());
-            char guess = player.getGuess();
-            game.Guess(guess);
-        }
-        System.out.println(hangman.report.showEnd());
+            while (game.isActive()) {
+                System.out.print(hangman.report.showReport());
+                char guess = player.getGuess();
+                game.Guess(guess);
+            }
+            //Knows if game is lost or won
+            System.out.println(hangman.report.showEnd());
+        } while (hangman.getChoiceFromPlayer(player));
     }
 
     private void setLanguageToPlayerChoice(Hangman hangman, Game game, Language language) throws FileNotFoundException {
@@ -36,24 +41,32 @@ public class Hangman {
         }
     }
 
-    private Language getLanguageFromPlayer(Player player) throws FileNotFoundException {
+    private Language getLanguageFromPlayer(Player player) {
         System.out.println("Which language do you prefer? Welke taal heeft uw preferentie? \n" +
                 "Typ \"Nederlands\" voor Nederlands or \"English\" for English.");
         while (true) {
-            String kiesTaal = player.chooseLanguage();
-            if (kiesTaal.contains("nederlands")) {
+            String languageChoice = player.getChoice();
+            if (languageChoice.contains("nederlands")) {
                 return Language.NEDERLANDS;
-            } else if (kiesTaal.contains("english")) {
+            } else if (languageChoice.contains("english")) {
                 return Language.ENGLISH;
             } else
                 System.out.println("Kies een van de twee bovenstaande talen. Choose one of the two languages above.");
         }
     }
 
+    private boolean getChoiceFromPlayer(Player player) {
+        System.out.println("Wil je opnieuw spelen? / Do you want to play again?");
+        System.out.println("Typ \"Ja\" of \"Nee\". / Type \"Yes\" or \"No\".");
+        while (true) {
+            String getChoice = player.getChoice();
+            if (getChoice.contains("ja") || getChoice.contains("yes")) {
+                return true;
+            }
+            if (getChoice.contains("nee") || getChoice.contains("no")) {
+                return false;
+            }
+        }
+    }
 }
-//interact with Player
-
-
-//Keeps track of Game
-//Knows if game is lost or won
 
