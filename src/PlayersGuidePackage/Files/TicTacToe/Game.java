3d1ab4c67a4aaba528d0037eb5166c938e5109e9
desireekorink <desireekorink@hotmail.com>
2023-09-19 13:59:18 +0200

    Created a constructor for Game(), changed isWin('X')|| isWin('O') to use the getSymbol(); Removed the Systen,out.println()'s from the isWin() method, created a checkWin() method, playerXhasWon() method and a playerOHasWon() method to create a check if someone has won. 2023-09-19

--- src/PlayersGuidePackage/Files/TicTacToe/Game.java
+++ src/PlayersGuidePackage/Files/TicTacToe/Game.java
@@ -1,17 +1,22 @@
 package PlayersGuidePackage.Files.TicTacToe;
 
 public class Game {
-    Grid grid = new Grid();
-    Player player;
-    Player playerX = new Player(player.getPlayerX(), 'X');
-    Player playerO = new Player(player.getPlayerO(), 'O');
+    private final Grid grid;
+    private final PlayerX playerX;
+    private final PlayerO playerO;
+
+    public Game() {
+        grid = new Grid(); // Initialize the grid here
+        playerX = new PlayerX("Player X", 'X');
+        playerO = new PlayerO("Player O", 'O');
+    }
 
     //game over
     public boolean isGameOver(){
-
-        return isWin('X')|| isWin('O') || isDraw();
-            
+        return isWin(playerX.getSymbol())|| isWin(playerO.getSymbol()) || isDraw();
     }
+
+
 
     private boolean isDraw() {
         // Check if all cells are filled (grid is full)
@@ -26,24 +31,36 @@ public class Game {
     }
 
     public boolean isWin(char symbol) {
-        // Check rows, columns, and diagonals for a win
         for (int i = 0; i < grid.size; i++){
             if (grid.grid[i][0] == symbol && grid.grid[i][1] == symbol && grid.grid[i][2] == symbol){
-                System.out.println("Congratulations " + (symbol == 'X' ? playerX : playerO) + ", you filled a whole ROW with " + symbol + " .");
                 return true; //Row Win
             }
             else if (grid.grid[0][i] == symbol && grid.grid[1][i] == symbol && grid.grid[2][i] == symbol){
-                System.out.println("Congratulations " + (symbol == 'X' ? playerX : playerO) + ", you filled a whole COLUMN with " + symbol + " .");
                 return true; // Column Win
             }
 
             else if ((grid.grid[0][0] == symbol && grid.grid[1][1] == symbol && grid.grid[2][2] == symbol ) ||
                     (grid.grid[0][2] == symbol && grid.grid[1][1] == symbol && grid.grid[2][0] == symbol)){
-                System.out.println("Congratulations " + (symbol == 'X' ? playerX : playerO) + ", you filled a DIAGONAL LINE with " + symbol + " .");
                 return true; //Diagonal wins
             }
         }
         return false;
+    }
+
+    public boolean checkWin(char symbol) {
+        return isWin(symbol);
+    }
+
+    public void playerXHasWon(){
+        if (checkWin(playerX.getSymbol())){
+            System.out.println(playerX.getName() + "has Won!");
+        }
+    }
+
+    public void playerOHasWon(){
+        if (checkWin(playerO.getSymbol())){
+            System.out.println(playerO.getName() + "has Won!");
+        }
     }
 
     //start new game
